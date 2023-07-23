da0f7bbf802870e89d92e23382c3b2c5d910ab60
believeRavi <114045526+believeRavi@users.noreply.github.com>
2023-07-23 10:28:29 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,71 +1,43 @@
-const backToTopButton = document.querySelector("#back-to-top-btn");
+// Add your javascript here
+// Don't forget to add it into respective layouts where this js file is needed
 
-window.addEventListener("scroll", scrollFunction);
+$(document).ready(function() {
+  AOS.init( {
+    // uncomment below for on-scroll animations to played only once
+    // once: true  
+  }); // initialize animate on scroll library
+});
 
-function scrollFunction() {
-  if (window.pageYOffset > 300) {
-    // Show backToTopButton
-    if (!backToTopButton.classList.contains("btnEntrance")) {
-      backToTopButton.classList.remove("btnExit");
-      backToTopButton.classList.add("btnEntrance");
-      backToTopButton.style.display = "block";
+// Smooth scroll for links with hashes
+$('a.smooth-scroll')
+.click(function(event) {
+  // On-page links
+  if (
+    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+    && 
+    location.hostname == this.hostname
+  ) {
+    // Figure out element to scroll to
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    // Does a scroll target exist?
+    if (target.length) {
+      // Only prevent default if animation is actually gonna happen
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000, function() {
+        // Callback after animation
+        // Must change focus!
+        var $target = $(target);
+        $target.focus();
+        if ($target.is(":focus")) { // Checking if the target was focused
+          return false;
+        } else {
+          $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+          $target.focus(); // Set focus again
+        };
+      });
     }
-  } else {
-    // Hide backToTopButton
-    if (backToTopButton.classList.contains("btnEntrance")) {
-      backToTopButton.classList.remove("btnEntrance");
-      backToTopButton.classList.add("btnExit");
-      setTimeout(function () {
-        backToTopButton.style.display = "none";
-      }, 250);
-    }
   }
-}
-
-backToTopButton.addEventListener("click", smoothScrollBackToTop);
-
-function smoothScrollBackToTop() {
-  const targetPosition = 0;
-  const startPosition = window.pageYOffset;
-  const distance = targetPosition - startPosition;
-  const duration = 750;
-  let start = null;
-
-  window.requestAnimationFrame(step);
-
-  function step(timestamp) {
-    if (!start) start = timestamp;
-    const progress = timestamp - start;
-    window.scrollTo(
-      0,
-      easeInOutCubic(progress, startPosition, distance, duration)
-    );
-    if (progress < duration) window.requestAnimationFrame(step);
-  }
-}
-
-function easeInOutCubic(t, b, c, d) {
-  t /= d / 2;
-  if (t < 1) return (c / 2) * t * t * t + b;
-  t -= 2;
-  return (c / 2) * (t * t * t + 2) + b;
-}
-
-const slides = document.querySelectorAll(".slide");
-
-let currentIndex = 0;
-
-setInterval(() => {
-  if (currentIndex == slides.length - 1) {
-    currentIndex = 0;
-    document.querySelector(".active").classList.remove("active");
-    slides[currentIndex].classList.add("active");
-    return;
-  }
-
-  if (currentIndex < slides.length) {
-    document.querySelector(".active").classList.remove("active");
-    currentIndex++;
-    slides[currentIndex].classList.add("active");
-  }
-}, 5000);
+});
